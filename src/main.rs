73eec0681d83b21416ff8e754ee85293255f26b6
Unai Hernandez Minaberry <unihernandez22@gmail.com>
2025-09-08 02:07:00 +0200

    Make the '/' key focus the search bar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use iced;
-use iced::widget::{button, column, text, row, text_input, Column};
+use iced::{self, Task};
+use iced::widget::{column, text, row, text_input, Column};
 use iced::Length::Fill;
+use iced::keyboard;
 
 #[derive(Default)]
 struct App {
@@ -10,36 +11,59 @@ struct App {
 #[derive(Debug, Clone)]
 enum Message {
     SearchInputChanged(String),
-    SearchButtonPressed,
+    FocusSearchInput,
 }
 
 impl App {
     fn view(&self) -> Column<'_, Message> {
+        let searchable_items = vec!["Cheese", "Milk", "Eggs", "Butter"];
+        let search_results =
+            if !self.search_input_string.is_empty() {
+                column(
+                    searchable_items
+                        .iter()
+                        .filter(|item| item.to_lowercase().contains(&self.search_input_string.to_lowercase()))
+                        .map(|item| text(*item).into())
+                )
+            } else {
+                column![]
+            };
+
         column![
             row![
                 text_input("Search...", &self.search_input_string)
+                    .id("search_input")
                     .on_input(Message::SearchInputChanged)
                     .width(Fill),
-                button("Search")
-                    .on_press(Message::SearchButtonPressed)
             ],
-            text(format!("You searched for: {}", self.search_input_string))
-                .width(Fill)
+            search_results,
         ]
     }
     
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::SearchInputChanged(s) => {
                 self.search_input_string = s;
-            }
-            Message::SearchButtonPressed => {
-                println!("Search button pressed with query: {}", self.search_input_string);
+                Task::none()
+            },
+            Message::FocusSearchInput => {
+                text_input::focus("search_input")
             }
         }
+    }
+    
+    fn subscription(&self) -> iced::Subscription<Message> {
+        keyboard::on_key_press(|key, _modifiers| {
+            match key.as_ref() {
+                keyboard::Key::Character("/") => Some(Message::FocusSearchInput),
+                _ => None,
+            }
+        })
     }
 }
 
 fn main() -> iced::Result {
-    iced::run("Pixel Editor", App::update, App::view)
+    iced::application("Pixel Editor", App::update, App::view)
+        .subscription(App::subscription)
+        .run()
 }
